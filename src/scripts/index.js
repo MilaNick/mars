47236bddda79973fb889e7a488dfeb13ed88dd71
Mila Zolotova <l.zolotova@yahoo.com>
2024-02-08 23:46:32 +0300

    added calendar, effect on the hover

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,71 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+    /* mouse movement - changing background-position-x */
+    // document.querySelector('video').playbackRate = 1.7;
+
+    const throttle = (fn, throttleTime) => {
+        let start = -Infinity;
+        let cachedResult;
+        return function () {
+            const end = Date.now();
+            if (end - start >= throttleTime) {
+                start = end;
+                cachedResult = fn.apply(this, arguments);
+            }
+            return cachedResult;
+        };
+    }
+
+    const moveBg = document.querySelector('.move-bg');
+    moveBg.addEventListener('mousemove', throttle((e) => {
+        const header = document.querySelector('.header');
+        const { y: headerY, height: headerHeight} = header.getBoundingClientRect();
+        const headerBottomY = headerY + headerHeight;
+        if (e.clientY < headerBottomY) {
+            return;
+        }
+        const centerBody = moveBg.getBoundingClientRect().width / 2;
+        const DEFAULT_POSITION = 50;
+        const offset = e.clientX - centerBody;
+        const newPosition = offset * DEFAULT_POSITION / centerBody + DEFAULT_POSITION;
+        moveBg.setAttribute("style", `background-position-x: ${newPosition}%`)
+    }, 110));
+
+    /* fullscreenPopup */
+    const fullscreenPopup = document.querySelector('.full-screen-popup');
+    const btnBuyTicket = document.querySelector('.header__btn');
+    const btnClosed = document.querySelector('.buy-tickets__closed');
+
+    btnBuyTicket.addEventListener('click', () => {
+        fullscreenPopup.classList.add('full-screen-popup--active');
+    })
+
+    btnClosed.addEventListener('click', () => {
+        fullscreenPopup.classList.remove('full-screen-popup--active');
+    })
+
+    /* js-datepicker */
+    const options = {
+        id: 1,
+        formatter: (input, date) => {
+            input.value = date.toLocaleDateString();
+        },
+        customDays: ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+        customMonths: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    }
+
+    datepicker('.buy-tickets-range[name="date-from"]', options);
+    datepicker('.buy-tickets-range[name="date-to"]', options);
+
+    /* for animation */
+
+    setTimeout(() => {
+        document.body.classList.add('active');
+    }, 100);
+
+}
+/* play/pause video onclick*/
 // const myVideo = document.getElementById("video");
 // myVideo.addEventListener('click', function(e){
 //     e.preventDefault();
@@ -9,43 +77,3 @@
 //         this.pause();
 //     }
 // });
-document.querySelector('video').playbackRate = 1.7;
-const throttle = (fn, throttleTime) => {
-    let start = -Infinity;
-    let cachedResult;
-    return function() {
-        const end = Date.now();
-        if (end - start >= throttleTime) {
-            start = end;
-            cachedResult = fn.apply(this, arguments);
-        }
-        return cachedResult;
-    };
-}
-
-const moveBg = document.querySelector('.move-bg');
-
-moveBg.addEventListener('mousemove', throttle((e) => {
-    const centerBody = moveBg.getBoundingClientRect().width / 2;
-    const DEFAULT_POSITION = 50;
-    const offset = e.clientX - centerBody;
-    const newPosition = offset * DEFAULT_POSITION / centerBody + DEFAULT_POSITION;
-    moveBg.setAttribute("style",`background-position-x: ${newPosition}%`)
-}, 110));
-
-const fullscreenPopup = document.querySelector('.wrapper');
-document.body.addEventListener('click', () => {
-    // fullscreenPopup.classList.add('wrapper--active');
-})
-
-const options = {
-    id: 1,
-    formatter: (input, date) => {
-        input.value = date.toLocaleDateString();
-    },
-    customDays: ['пн', 'вт', '二', '三', '四', '五', '六'],
-    customMonths: ['Январь', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-}
-
-const start = datepicker('.buy-tickets-range[name="date-from"]', options);
-const end = datepicker('.buy-tickets-range[name="date-to"]', options);
